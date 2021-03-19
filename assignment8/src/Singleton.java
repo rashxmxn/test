b6d41e4a57cd77bb2f9c8f8b854b86c50aac22a7
rashxmxn <yunglaflame3@gmail.com>
2021-03-19 11:30:31 +0600

    added 8th assignment of OOP Java hometask

---
+++ assignment8/src/Singleton.java
@@ -0,0 +1,9 @@
+public class Singleton {
+    private Singleton()
+    {}
+    public String str;
+    private static Singleton instance = new Singleton();
+    public static Singleton getSingleInstance(){
+        return instance;
+    }
+}
